a25a76516becf52a71ac6de75ed5887d53ed4c3f
oahnukuw <oahnukuw@gmail.com>
2025-04-16 16:39:00 +0800

    Union-Find DS Realization

---
+++ src/main/java/leetcode/editor/ds/UF.java
@@ -0,0 +1,37 @@
+package leetcode.editor.ds;
+
+public class UF {
+    private int[] parent;
+    private int unionCount;
+
+    public UF(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        unionCount = n;
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootQ == rootP) {
+            return;
+        }
+        parent[rootP] = rootQ;
+        unionCount--;
+    }
+
+    public boolean connected(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        return rootQ == rootP;
+    }
+
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+}
